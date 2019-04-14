d4919247e4dcfa4a264305934e5b41b7e9626d44
Crystian Jordan <crystian.jordan@catolicasc.org.br>
2019-04-14 20:14:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pokequiz/PokemonMaster.java
@@ -0,0 +1,33 @@
+package com.example.pokequiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PokemonMaster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_master);
+        TextView rightCount;
+        Button play;
+        rightCount = findViewById(R.id.acertos);
+        play = findViewById(R.id.playAgain);
+
+        Intent i = getIntent();
+        String right = i.getStringExtra("acertos");
+rightCount.setText("Acertos: "+right+"/30");
+
+play.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(PokemonMaster.this,MainActivity.class);
+        startActivity(i);
+    }
+});
+    }
+}
